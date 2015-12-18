c7b856e5616b2631e93383ea9109bca88e94244c
elaine <ra1695@nyu.edu>
2015-12-18 03:41:12 +0000

    add geo verification for post

--- LocalNotes/noteboard/views.py
+++ LocalNotes/noteboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
-
+from django.contrib import messages
 from django.contrib.gis.geoip2 import GeoIP2
 
 from .models import *
@@ -21,7 +21,7 @@ def search(request):
 	if request.method == 'POST':
 		cities_list = City.objects.filter(name__contains = request.POST.get('keyword'))[:5]
 		context = {'cities_list': cities_list}		
-	request.session.flush()				
+
 	return render(request, 'noteboard/search.html', context)
 
 class CitiesView(generic.ListView):
@@ -52,28 +52,48 @@ class CityView(generic.DetailView):
 		return context
 
 def post(request):
-	# host = request.META.get('REMOTE_HOST')
-	ip_address = request.META.get('REMOTE_ADDR')
 
-	geo = GeoIP2()
-	current_city = geo.city(str(ip_address))
-	city_name = str(current_city['city'])
-	country_name = str(current_city['country_name'])
+    ip_address = request.META.get('REMOTE_ADDR')
 
-	if len(City.objects.filter(name=city_name)) == 0:
-		user_city = City(name=city_name, country=country_name, summary="Please add summary")
-		user_city.save()
-	else:
-		user_city = City.objects.get(name=city_name)
-	if request.method == 'POST':
-		selected_post = Post(
-						title = request.POST.get('title'),
-						body = request.POST.get('body'),
-						city = user_city,
-						user = User.objects.get(id = request.session['member_id']))
-		selected_post.save()
-		
-	return HttpResponseRedirect(reverse('noteboard:CityView', args=(user_city.pk,)))
+    geo = GeoIP2()
+    current_city = geo.city(str(ip_address))
+    city_name = str(current_city['city'])
+    country_name = str(current_city['country_name'])
+
+    input_city_country = request.POST.get('city')
+
+    if input_city_country == "":
+        user_city = City(name=city_name, country=country_name, summary="Please add summary")
+        user_city.save()
+        input_city = city_name
+        input_country = country_name
+        
+    else:
+        input_city = str(input_city_country.split("+")[0])
+        input_country = str(input_city_country.split("+")[1])
+        print "user inputs " + input_city + "," + input_country
+
+    if input_city == city_name and country_name == input_country:
+        user_city = City.objects.get(name=city_name)
+        if request.method == 'POST':
+            selected_post = Post(
+                            title = request.POST.get('title'),
+                            body = request.POST.get('body'),
+                            city = user_city,
+                            user = User.objects.get(id = request.session['member_id']))
+            selected_post.save()
+
+        return HttpResponseRedirect(reverse('noteboard:CityView', args=(user_city.pk,)))
+
+    else:
+        return error(request, "The city selected does not match the current city you are in, please re-add post.")
+
+def error(request, err_message):
+    context = dict()
+    context['messages'] = err_message
+    context['user'] = User.objects.get(id = request.session['member_id'])
+    return render(request, 'noteboard/city_error.html', context)
+
 	
 class UserView(generic.DetailView):
 	model = User
