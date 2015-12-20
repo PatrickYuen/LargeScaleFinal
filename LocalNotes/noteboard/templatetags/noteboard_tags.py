1a270e65020ce33573f6bfd5ac2fb5a7bb280b8e
elaine <ra1695@nyu.edu>
2015-12-20 06:07:17 +0000

    cache...

--- LocalNotes/noteboard/templatetags/noteboard_tags.py
+++ LocalNotes/noteboard/templatetags/noteboard_tags.py
@@ -9,9 +9,10 @@ register = template.Library()
 @register.simple_tag
 def get_all_cities():
     all_cities = cache.get("cities")
+    print all_cities
     if all_cities is None:
         all_cities = City.objects.all()
-        cache.set("cities", all_cities, 2*60)
+        cache.set("cities", all_cities, 5*60)
 
     return all_cities
 
