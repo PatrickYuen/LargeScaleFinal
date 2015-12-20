07ca2c524dc94f905e75324d337a2c351cf3f40a
Elaine <ra1695@nyu.edu>
2015-12-19 23:06:40 -0500

    add cache for cities

--- LocalNotes/noteboard/templatetags/noteboard_tags.py
+++ LocalNotes/noteboard/templatetags/noteboard_tags.py
@@ -2,10 +2,16 @@ __author__ = 'elaine'
 
 from ..models import *
 from django import template
+from django.core.cache import cache
 
 register = template.Library()
 
 @register.simple_tag
 def get_all_cities():
-    return City.objects.all()
+    all_cities = cache.get("cities", "expired")
+    if all_cities == "expired":
+        all_cities = City.objects.all()
+        cache.set("cities", all_cities, 60*60)
+
+    return all_cities
 
